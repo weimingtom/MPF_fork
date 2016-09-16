40b18a16fbbde2bf161c8726779d81c4132c9eb5
china0851 <china0851@6fc91ae5-6b3c-4328-b870-8ec626a46ab3>
2016-09-16 23:33:05 +0000

    *)修复资源

--- uidesigner/MPFAssist10/AddIn_i.c
+++ uidesigner/MPFAssist10/AddIn_i.c
@@ -6,7 +6,7 @@
 
 
  /* File created by MIDL compiler version 7.00.0555 */
-/* at Sat Sep 17 07:15:53 2016
+/* at Sat Sep 17 07:29:32 2016
  */
 /* Compiler settings for AddIn.idl:
     Oicf, W1, Zp8, env=Win32 (32b run), target_arch=X86 7.00.0555 
