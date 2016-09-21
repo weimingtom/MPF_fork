f3c25c76f42f7c57c86efe32346960e0309d0bff
china0851 <china0851@6fc91ae5-6b3c-4328-b870-8ec626a46ab3>
2016-09-21 05:40:59 +0000

    +)增加创建VS工程向导界面逻辑

--- uidesigner/uidframe/src/Main/VSWindow.cpp
+++ uidesigner/uidframe/src/Main/VSWindow.cpp
@@ -1,278 +1,104 @@
 /////////////////////////////////////////////////////
-// StartWindow.cpp
+// VSWindow.cpp
 
 #include "stdafx.h"
-#include "StartWindow.h"
+#include "VSWindow.h"
 #include <Core/ResNode.h>
 #include <Tools/Utils.h>
+#include <main/VSManager.h>
 
-StartWindow::StartWindow()
-{
-    _bCanContinue = false;
-    _recentlyPrjs.setAutoDelete(false);
-}
-
-StartWindow::~StartWindow()
+VSWindow::VSWindow()
 {
 }
 
-void StartWindow::OnOpenProjectDir(suic::Element* sender, suic::RoutedEventArg* e)
+VSWindow::~VSWindow()
+{
+}
+
+void VSWindow::StartVSCreator()
+{
+    VSWindow vsWnd;
+    suic::String strUri(_U("/mpfuid;/resource/uidesign/layout/VSWindow.xaml"));
+
+    vsWnd.setAutoDelete(false);
+    vsWnd.ShowDialog(strUri);
+}
+
+void VSWindow::OnOpenDirButtonClick(suic::DpObject* sender, suic::RoutedEventArg* e)
 {
     e->SetHandled(true);
+    suic::DirBrowser dirOpen;
+    suic::TextBox* prjDir = FindElem<suic::TextBox>(_U("PrjDir"));
 
-    suic::DirBrowser fb;
-    if (fb.Open(this))
+    if (dirOpen.Open())
     {
-        _newPrjPath = fb.GetDir();
-        suic::TextBox* txtBox = FindElem<suic::TextBox>(_U("PrjPath"));
-        if (NULL != txtBox)
-        {
-            txtBox->SetText(_newPrjPath);
-        }
-        //AsyncClose();
+        prjDir->SetText(dirOpen.GetDir());
     }
 }
 
-void StartWindow::OnNewProject(suic::Element* sender, suic::RoutedEventArg* e)
+void VSWindow::OnButtonClick(suic::DpObject* sender, suic::RoutedEventArg* e)
 {
+    suic::Element* pElem = suic::RTTICast<suic::Element>(sender);
+    suic::TextBox* prjName = FindElem<suic::TextBox>(_U("PrjName"));
+    suic::TextBox* prjDir = FindElem<suic::TextBox>(_U("PrjDir"));
+
+    suic::String strName = prjName->GetText();
+    suic::String strDir = prjDir->GetText();
+
+    strName.Trim();
+    strDir.Trim();
+
     e->SetHandled(true);
 
-    suic::TextBox* titleBox = FindElem<suic::TextBox>(_U("PrjName"));
-    suic::TextBox* txtBox = FindElem<suic::TextBox>(_U("PrjPath"));
-
-    if (NULL != txtBox && NULL != titleBox)
+    if (strName.Empty())
     {
-        _prjName = titleBox->GetText();
-        _newPrjPath = txtBox->GetText();
-    }
-
-    if (!_prjName.Empty() && !_newPrjPath.Empty())
-    {
-        _bCanContinue = true;
-        _openPrjPath = _U("");
-        AsyncClose();
-    }
-    else
-    {
-        suic::InfoBox::Show(_T("请输入项目名称或路径!"), _T("提示"));
-    }
-}
-
-void StartWindow::OnOpenProject(suic::Element* sender, suic::RoutedEventArg* e)
-{
-    e->SetHandled(true);
-
-    suic::FileBrowser fb;
-    if (fb.Open(this, _U("All Files(*.uiproj)\0*.uiproj\0\0")))
-    {
-        _openPrjPath = fb.GetFilePath();
-        _bCanContinue = true;
-        AsyncClose();
-    }
-}
-
-void StartWindow::OnCloseStartWindow(suic::Element* sender, suic::RoutedEventArg* e)
-{
-    e->SetHandled(true);
-    AsyncClose();
-}
-
-void StartWindow::OnDbclkOnItem(suic::Element* sender, suic::MouseButtonEventArg* e)
-{
-    if (e->GetMouseButton() != suic::MouseButton::mbLeftButton)
-    {
+        suic::Toast::Show(_U("请输入工程名称！"), Toast::DelayClose::ShortTime);
         return;
     }
 
-    Element* elem = RTTICast<Element>(e->GetOriginalSource());
-    RecentlyPrj* item = DynamicCast<RecentlyPrj>(elem->GetDataContext());
-    if (NULL != item)
+    if (strDir.Empty())
     {
-        e->SetHandled(true);
-        _bCanContinue = true;
-        _openPrjPath = item->path;
-        AsyncClose();
+        suic::Toast::Show(_U("请输入工程路径！"), Toast::DelayClose::ShortTime);
+        return;
     }
-}
 
-bool StartWindow::OnBuild(suic::IXamlNode* pNode, suic::ObjectPtr& obj)
-{
-    return false;
-}
-
-void StartWindow::OnConnect(suic::IXamlNode* pNode, suic::Object* target)
-{
-}
-
-void StartWindow::AddRecentlyProject(const String& name, const String& path)
-{
-    if (!name.Empty() && !path.Empty())
+    if (strName.Equals(_U("OKBtn")))
     {
-        for (int i = 0; i < _recentlyPrjs.GetCount(); ++i)
-        {
-            RecentlyPrj* item = DynamicCast<RecentlyPrj>(_recentlyPrjs.GetItem(i));
-            String name1 = item->name;
-            String name2 = name;
-
-            if (name1.CompareI(name2) == 0)
-            {
-                return;
-            }
-        }
-        _recentlyPrjs.AddItem(new RecentlyPrj(name, path));
-    }
-}
-
-void StartWindow::ReadRecentlyProjects()
-{
-    XmlDoc xmlDoc;
-    XmlNode* root = NULL;
-
-    try
-    {
-        suic::Char appPath[512];
-        ::GetModuleFileName(NULL, appPath, 511);
-        suic::Mulstr strPath(appPath);
-
-        strPath += ".xml";
-        suic::Mulstr buff;
-
-        Utils::ReadResFromFile(Mulstr(strPath.c_str()).c_str(), "rb", buff);
-        buff = buff.FromUtf8(buff.c_str(), buff.Length());
-
-        xmlDoc.parse<0>(buff.c_str());
-        root = xmlDoc.first_node();
-
-        if (NULL == root)
+        if (_checkedVs.Empty())
         {
             return;
         }
 
-        XmlNode* childNode = root->first_node();
-        while (childNode != NULL)
-        {
-            String strName(childNode->name());
-            if (strName.Equals(_U("Recently")))
-            {
-                XmlNode* recNode = childNode->first_node();
-                while (recNode != NULL)
-                {
-                    String name;
-                    String path;
-
-                    XmlAttr* attr = recNode->first_attribute("Name");
-
-                    if (NULL != attr)
-                    {
-                        name = attr->value();
-                    }
-
-                    attr = recNode->first_attribute("Path");
-
-                    if (NULL != attr)
-                    {
-                        path = suic::FileDir::CalculatePath(attr->value());
-                    }
-
-                    if (!name.Empty() && !path.Empty() && FileDir::GetFileProp(path) == FileDir::fpFile)
-                    {
-                        RecentlyPrj* item = new RecentlyPrj(name, path);
-                        _recentlyPrjs.AddItem(item);
-                    }
-
-                    recNode = recNode->next_sibling();
-                }
-            }
-
-            childNode = childNode->next_sibling();
-        }
+        VSManager::CreateVSProject(_checkedVs, strName, strDir);
     }
-    catch (char* err)
-    {
-        ;
-    }
-    catch (...)
-    {
-    }
+    AsyncClose();
 }
 
-void StartWindow::SaveRecentlyProjects()
+void VSWindow::OnVSButtonClick(suic::DpObject* sender, suic::RoutedEventArg* e)
 {
-    suic::String strXml;
-    suic::Char appPath[512];
-    ::GetModuleFileName(NULL, appPath, 511);
-    suic::String strPath(appPath);
+    suic::RadioButton* pElem = suic::RTTICast<suic::RadioButton>(sender);
+    suic::String strName = pElem->GetName();
 
-    strPath += _U(".xml");
+    _checkedVs = pElem->GetContent()->ToString();
 
-    strXml += _T("<?xml version=\"1.0\" encoding=\"utf-8\"?>\n");
-    strXml += _U("<UIConfig>\n");
-
-    strXml += ResNode::OFFSET1 + _U("<Recently>\n");
-
-    for (int i = 0; i < _recentlyPrjs.GetCount(); ++i)
-    {
-        RecentlyPrj* item = DynamicCast<RecentlyPrj>(_recentlyPrjs.GetItem(i));
-
-        strXml += ResNode::OFFSET2 + _U("<Item Name=\"");
-        strXml += item->name;
-        strXml += _U("\" Path=\"");
-        strXml += item->path;
-        strXml += _U("\"/>\n");
-    }
-    strXml += ResNode::OFFSET1 + _U("</Recently>\n");
-
-    strXml += _U("</UIConfig>\n");
-
-    FileWriter fs;
-
-    if (fs.Open(strPath))
-    {
-        fs.Write(strXml);
-        fs.Close();
-    }
+    e->SetHandled(true);
 }
 
-void StartWindow::OnInitialized(EventArg* e)
+bool VSWindow::OnBuild(suic::IXamlNode* pNode, suic::ObjectPtr& obj)
+{
+    return false;
+}
+
+void VSWindow::OnConnect(suic::IXamlNode* pNode, suic::Object* target)
+{
+}
+
+void VSWindow::OnInitialized(EventArg* e)
 {
     suic::Window::OnInitialized(e);
 }
 
-void StartWindow::OnLoaded(suic::LoadedEventArg* e)
+void VSWindow::OnLoaded(suic::LoadedEventArg* e)
 {
     suic::Window::OnLoaded(e);
-
-    ReadRecentlyProjects();
-
-    ListBox* recList = FindElem<suic::ListBox>(_U("RecentlyPrjs"));
-
-    if (NULL != recList)
-    {
-        recList->SetItemsSource(&_recentlyPrjs);
-        recList->AddPreviewMouseDoubleClick(new MouseButtonEventHandler(this, &StartWindow::OnDbclkOnItem));
-    }
-
-    suic::Button* pBtn = FindElem<suic::Button>(_U("OpenDir"));
-    if (NULL != pBtn)
-    {
-        pBtn->AddClick(new suic::ClickEventHandler(this, &StartWindow::OnOpenProjectDir));
-    }
-
-    pBtn = FindElem<suic::Button>(_U("NewPrjOk"));
-    if (NULL != pBtn)
-    {
-        pBtn->AddClick(new suic::ClickEventHandler(this, &StartWindow::OnNewProject));
-    }
-
-    pBtn = FindElem<suic::Button>(_U("OpenProject"));
-    if (NULL != pBtn)
-    {
-        pBtn->AddClick(new suic::ClickEventHandler(this, &StartWindow::OnOpenProject));
-    }
-
-    pBtn = FindElem<suic::Button>(_U("CloseBtn"));
-    if (NULL != pBtn)
-    {
-        pBtn->AddClick(new suic::ClickEventHandler(this, &StartWindow::OnCloseStartWindow));
-    }
 }
--- uidesigner/uidframe/src/Main/VSWindow.h
+++ uidesigner/uidframe/src/Main/VSWindow.h
@@ -1,83 +1,38 @@
 // 华勤科技版权所有 2010-2011
 // 
-// 文件名：StartWindow.h
-// 功  能：启动界面窗口。
+// 文件名：VSWindow.h
+// 功  能：VS工程向导界面窗口。
 // 
 // 作  者：汪荣
 // 时  间：2013-01-02
 // 
 // ============================================================================
 
-# ifndef _STARTWINDOW_H_
-# define _STARTWINDOW_H_
+# ifndef _VSWINDOW_H_
+# define _VSWINDOW_H_
 
 using namespace suic;
 
-class RecentlyPrj : public Object
+class VSWindow : public suic::Window
 {
 public:
 
-    String name;
-    String path;
+    BeginRoutedEvent(VSWindow, suic::Window)
+        MemberRouted(OnButtonClick)
+        MemberRouted(OnVSButtonClick)
+        MemberRouted(OnOpenDirButtonClick)
+    EndRoutedEvent()
 
-    RecentlyPrj()
-    {
+    VSWindow();
+    ~VSWindow();
 
-    }
-
-    RecentlyPrj(const String& n, const String& p)
-    {
-        name = n;
-        path = p;
-    }
-
-    String ToString()
-    {
-        return name;
-    }
-};
-
-class StartWindow : public suic::Window
-{
-public:
-
-    StartWindow();
-    ~StartWindow();
-
-    suic::String GetProjectName() const
-    {
-        return _prjName;
-    }
-
-    suic::String GetNewProjectPath() const
-    {
-        return _newPrjPath;
-    }
-
-    suic::String GetOpenProjectPath() const
-    {
-        return _openPrjPath;
-    }
-
-    bool IsCanContinue() const
-    {
-        return _bCanContinue;
-    }
-
-    void SaveRecentlyProjects();
-    void AddRecentlyProject(const String& name, const String& path);
-
-    void OnNewProject(suic::Element* sender, suic::RoutedEventArg* e);
-    void OnOpenProjectDir(suic::Element* sender, suic::RoutedEventArg* e);
-
-    void OnOpenProject(suic::Element* sender, suic::RoutedEventArg* e);
-    void OnCloseStartWindow(suic::Element* sender, suic::RoutedEventArg* e);
-
-    void OnDbclkOnItem(suic::Element* sender, suic::MouseButtonEventArg* e);
+    static void StartVSCreator();
 
 protected:
 
-    void ReadRecentlyProjects();
+    void OnButtonClick(suic::DpObject* sender, suic::RoutedEventArg* e);
+    void OnVSButtonClick(suic::DpObject* sender, suic::RoutedEventArg* e);
+    void OnOpenDirButtonClick(suic::DpObject* sender, suic::RoutedEventArg* e);
 
     void OnInitialized(EventArg* e);
     void OnLoaded(suic::LoadedEventArg* e);
@@ -86,15 +41,9 @@ protected:
     bool OnBuild(suic::IXamlNode* pNode, suic::ObjectPtr& obj);
     void OnConnect(suic::IXamlNode* pNode, suic::Object* target);
 
-protected:
+private:
 
-    suic::String _prjName;
-    suic::String _openPrjPath;
-    suic::String _newPrjPath;
-
-    bool _bCanContinue;
-
-    suic::ObservableCollection _recentlyPrjs;
+    suic::String _checkedVs;
 };
 
 #endif
